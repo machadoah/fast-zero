0d91d200456259be546e9a3f13fcddc53c531c6f
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-07-31 02:27:12 -0300

    :sparkles: feat: Personalizando retorno do read_root com status_code e Message

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,11 +1,14 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
 from fast_zero.components.health_check import generate_health_check
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 async def read_root() -> dict:
     """
     Lê o endpoint raiz e retorna o nome do proprietário.
