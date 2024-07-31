21bfcc6e45f20e91dff6d6c5d10da0b3c1cf4fad
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-07-31 02:27:38 -0300

    :sparkles: feat: Criando endpoint html

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 from fast_zero.components.health_check import generate_health_check
 from fast_zero.schemas import Message
@@ -21,7 +22,29 @@ async def read_root() -> dict:
     return {'owner': '@machadoah'}
 
 
-@app.get('/health')
+@app.get('/html', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+async def read_root_html() -> str:
+    """
+    Lê o endpoint raiz e retorna o nome do proprietário.
+
+    :return:
+    Um dicionário/json com o @ do owner.
+
+    :rtype: str
+    """
+    return """
+    <html>
+        <head>
+            <title>Fast-Zero :zap:</title>
+        </head>
+        <body>
+            <h1>@machadoah</h1>
+        </body>
+    </html>
+    """
+
+
+@app.get('/health', status_code=HTTPStatus.OK, response_model=Message)
 async def health_check() -> dict:
     """
     Verifica o estado da API e retorna uma mensagem
