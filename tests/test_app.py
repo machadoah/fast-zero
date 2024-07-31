014bb7a8e330a314f6a11e3d265b287f989e9b31
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-07-31 01:07:59 -0300

    :test_tube: test: Criando primeiros testes

---
+++ tests/test_app.py
@@ -0,0 +1,23 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_zero.app import app
+
+
+def test_read_root_deve_retornar_ok_e_owner():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get('/')  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
+    assert response.json() == {'owner': '@machadoah'}  # Assert (Afirmação)
+
+
+def test_health_check_deve_retornar_ok_e_mensagem():
+    client = TestClient(app)
+
+    response = client.get('/health')
+
+    assert response.status_code == HTTPStatus.OK
+    assert ('API' in response.json()['message'].upper())
