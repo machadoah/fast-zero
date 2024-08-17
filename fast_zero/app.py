91d987c79c015e4fa71c43b577a4492344a8ecd4
Antonio Henrique Machado <antoniohenriquemachado08@gmail.com>
2024-08-17 15:39:28 -0300

    :wastebasket: remove: Removendo funcionalidade de GenAI

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -2,34 +2,9 @@ from http import HTTPStatus
 
 from fastapi import FastAPI
 
-from fast_zero.components.health_check import generate_health_check
-from fast_zero.schemas import Message
-
 app = FastAPI()
 
 
 @app.get('/', status_code=HTTPStatus.OK)
-async def read_root() -> dict:
-    """
-    Lê o endpoint raiz e retorna o nome do proprietário.
-
-    :return:
-    Um dicionário/json com o @ do owner.
-
-    :rtype: dict
-    """
+def read_root():
     return {'owner': '@machadoah'}
-
-
-@app.get('/health', status_code=HTTPStatus.OK, response_model=Message)
-async def health_check() -> dict:
-    """
-    Verifica o estado da API e retorna uma mensagem
-    indicando se está funcionando corretamente.
-
-    :return:
-    Uma mensagem gerada por um modelo de linguagem (LLM).
-
-    :rtype: dict
-    """
-    return {'message': generate_health_check()}
