eb37945e76b86b51c287e0d8da0d1a8babb8854b
Antonio Machado <antonio.machado@gupy.com.br>
2024-09-05 23:51:18 -0300

    ✨ feat: desenvolvendo endpoints do CRUD

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,10 +1,61 @@
 from http import HTTPStatus
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from fast_zero.schemas import Message, UserDB, UserList, UserPublic, UserSchema
 
 app = FastAPI()
 
 
-@app.get('/', status_code=HTTPStatus.OK)
+database = []
+
+
+@app.get(path='/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'owner': '@machadoah'}
+    return {'message': '@machadoah'}
+
+
+@app.post(path='/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(id=len(database) + 1, **user.model_dump())
+
+    database.append(user_with_id)
+
+    return user_with_id
+
+
+@app.get(path='/users/', status_code=HTTPStatus.OK, response_model=UserList)
+def read_users():
+    return {'users': database}
+
+
+@app.get(path='/users/{user_id}', status_code=HTTPStatus.OK, response_model=UserPublic)
+def read_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found 💥')
+
+    for user in database:
+        if user.id == user_id:
+            return user
+
+
+@app.put(path='/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: UserSchema):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found 💥')
+
+    user_with_id = UserDB(id=user_id, **user.model_dump())
+    database[user_id - 1] = user_with_id
+
+    return user_with_id
+
+
+@app.delete(path='/users/{user_id}', response_model=Message)
+def delete_user(user_id: int):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail='User not found 💥')
+
+    id_deleted = user_id
+    del database[user_id - 1]
+
+    return {'message': f'User {id_deleted} deleted 🗑️'}
