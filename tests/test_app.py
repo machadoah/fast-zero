bd128f39728200e78fb600a21ae41cb9216bd506
Antonio Machado <antonio.machado@gupy.com.br>
2024-09-05 23:49:15 -0300

    🧪 test: criando testes CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,24 +1,99 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
-
-
-def test_read_root_deve_retornar_ok_e_owner():
-    client = TestClient(app)  # Arrange (organização)
-
+def test_read_root(client):
     response = client.get('/')  # Act (ação)
 
     assert response.status_code == HTTPStatus.OK  # Assert (Afirmação)
-    assert response.json() == {'owner': '@machadoah'}  # Assert (Afirmação)
+    assert response.json() == {'message': '@machadoah'}  # Assert (Afirmação)
 
 
-def test_health_check_deve_retornar_ok_e_mensagem():
-    client = TestClient(app)
+def test_create_user(client):
+    response = client.post(
+        url='/users/',
+        json={
+            'username': 'machadoah',
+            'password': 'password',
+            'email': 'machado@ah.com',
+        },
+    )
 
-    response = client.get('/health')
+    # RETORNOU 201?
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        'username': 'machadoah',
+        'id': 1,
+        'email': 'machado@ah.com',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
 
     assert response.status_code == HTTPStatus.OK
-    assert 'API' in response.json()['message'].upper()
-    assert 'FUNCIONANDO' in response.json()['message'].upper()
+
+    assert response.json() == {'users': [{'id': 1, 'username': 'machadoah', 'email': 'machado@ah.com'}]}
+
+
+def test_read_user(client):
+    response = client.get('/users/1')
+    assert response.status_code == HTTPStatus.OK
+
+    assert response.json() == {'id': 1, 'username': 'machadoah', 'email': 'machado@ah.com'}
+
+
+def test_read_user_not_found(client):
+    response = client.get('/users/98')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        'detail': 'User not found 💥',
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'machadoah.tech',
+            'password': 'senha',
+            'email': 'machado@machadoah.tech',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'machadoah.tech',
+        'id': 1,
+        'email': 'machado@machadoah.tech',
+    }
+
+
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/98',
+        json={
+            'username': 'machadoah.tech',
+            'password': '<PASSWORD>',
+            'email': 'machado@machadoah.tech',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        'detail': 'User not found 💥',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User 1 deleted 🗑️'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete('/users/98')
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {
+        'detail': 'User not found 💥',
+    }
